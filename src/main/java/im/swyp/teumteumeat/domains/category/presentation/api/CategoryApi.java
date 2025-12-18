8e58d8d5a05ae7efc0a4af768c017e1186961f6d
hyotatoFrappuccino <hjkiln@naver.com>
2025-12-18 14:11:55 +0900

    📝 docs: 카테고리 API 명세서 추가

---
+++ src/main/java/im/swyp/teumteumeat/domains/category/presentation/api/CategoryApi.java
@@ -0,0 +1,77 @@
+package im.swyp.teumteumeat.domains.category.presentation.api;
+
+import im.swyp.teumteumeat.domains.category.application.dto.request.CategoryCreateRequest;
+import im.swyp.teumteumeat.domains.category.application.dto.request.CategoryUpdateRequest;
+import im.swyp.teumteumeat.domains.category.application.dto.response.CategoryListResponse;
+import im.swyp.teumteumeat.global.annotation.swagger.ApiResponseExplanations;
+import im.swyp.teumteumeat.global.annotation.swagger.ApiSuccessResponseExplanation;
+import im.swyp.teumteumeat.global.common.ApiResponse;
+import im.swyp.teumteumeat.global.security.dto.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "Category", description = "카테고리 API")
+public interface CategoryApi {
+
+    @Operation(
+            summary = "전체 카테고리 목록 조회"
+    )
+    @ApiResponseExplanations(
+            success = @ApiSuccessResponseExplanation(
+                    responseClass = CategoryListResponse.class,
+                    description = "조회 성공"
+            )
+    )
+    ResponseEntity<ApiResponse<CategoryListResponse>> getCategories(
+            @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails user
+    );
+
+    @Operation(
+            summary = "카테고리 추가",
+            description = "관리자(ADMIN)만 추가할 수 있습니다."
+    )
+    @ApiResponseExplanations(
+            success = @ApiSuccessResponseExplanation(
+                    description = "추가 성공"
+            )
+    )
+    ResponseEntity<ApiResponse<Void>> createCategory(
+            @RequestBody @Valid CategoryCreateRequest request,
+            @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails user
+    );
+
+    @Operation(
+            summary = "카테고리 수정",
+            description = "관리자(ADMIN)만 수정할 수 있습니다."
+    )
+    @ApiResponseExplanations(
+            success = @ApiSuccessResponseExplanation(
+                    description = "수정 성공"
+            )
+    )
+    ResponseEntity<ApiResponse<Void>> updateCategory(
+            @NotNull Long categoryId,
+            @RequestBody @Valid CategoryUpdateRequest request,
+            @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails user
+    );
+
+    @Operation(
+            summary = "카테고리 삭제",
+            description = "관리자(ADMIN)만 삭제할 수 있습니다."
+    )
+    @ApiResponseExplanations(
+            success = @ApiSuccessResponseExplanation(
+                    description = "삭제 성공"
+            )
+    )
+    ResponseEntity<ApiResponse<Void>> deleteCategory(
+            @NotNull Long categoryId,
+            @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails user
+    );
+}
