6dddcd45e6d4a3465ad22ec29659ee3dd815604e
jinnieusLab <kitty14904@naver.com>
2025-12-20 03:21:46 +0900

    :wrench: chore: LLM 응답 json 형식 조정

--- src/main/java/im/swyp/teumteumeat/domains/document/domain/service/DocumentLLMService.java
+++ src/main/java/im/swyp/teumteumeat/domains/document/domain/service/DocumentLLMService.java
@@ -7,6 +7,7 @@ import im.swyp.teumteumeat.domains.llm.domain.prompt.QuizPrompt;
 import im.swyp.teumteumeat.domains.llm.domain.service.LLMService;
 import im.swyp.teumteumeat.domains.quiz.domain.service.QuizService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,8 +32,11 @@ public class DocumentLLMService {
         String documentContent = document.getRawContent();
         int difficulty = 3; // 난이도 임시 고정
 
+        BeanOutputConverter<LLMResponse> converter = new BeanOutputConverter<>(LLMResponse.class);
+
         String prompt = String.format(QuizPrompt.GENERATE_QUIZ.getTemplate(), categoryName, documentContent,
-                difficulty);
+                difficulty) + "\n반드시 다음 JSON 스키마에 맞는 '데이터만' JSON 객체로 출력하세요 (스키마 정의나 metadata 포함 금지):\n"
+                + converter.getFormat();
         LLMResponse response = llmService.generateAnswer(prompt);
 
         response.quizzes().forEach(quizDto -> {
