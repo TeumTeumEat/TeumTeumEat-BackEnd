9668da2b228ce7e0142a087eae96edd32a42d4fb
hyotatoFrappuccino <hjkiln@naver.com>
2025-12-23 15:13:08 +0900

    ✨ feat: 디바이스 토큰 API 구현

---
+++ src/main/java/im/swyp/teumteumeat/domains/notification/application/usecase/DeviceTokenUseCase.java
@@ -0,0 +1,37 @@
+package im.swyp.teumteumeat.domains.notification.application.usecase;
+
+import im.swyp.teumteumeat.domains.notification.application.mapper.DeviceTokenMapper;
+import im.swyp.teumteumeat.domains.notification.domain.service.DeviceTokenService;
+import im.swyp.teumteumeat.domains.notification.persistence.entity.DeviceToken;
+import im.swyp.teumteumeat.domains.user.domain.service.UserService;
+import im.swyp.teumteumeat.domains.user.persistence.entity.UserEntity;
+import im.swyp.teumteumeat.global.annotation.UseCase;
+import im.swyp.teumteumeat.domains.notification.application.dto.request.DeviceTokenRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@UseCase
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class DeviceTokenUseCase {
+
+    private final DeviceTokenService deviceTokenService;
+    private final UserService userService;
+
+    @Transactional
+    public void registerDeviceToken(Long userId, DeviceTokenRequest request) {
+        UserEntity user = userService.getUserById(userId);
+
+        DeviceToken deviceToken = DeviceTokenMapper.toDeviceToken(user, request);
+        deviceTokenService.registerDeviceToken(deviceToken);
+    }
+
+    @Transactional
+    public void unregisterDeviceToken(Long userId, @Valid DeviceTokenRequest request) {
+        UserEntity user = userService.getUserById(userId);
+
+        DeviceToken deviceToken = DeviceTokenMapper.toDeviceToken(user, request);
+        deviceTokenService.unregisterDeviceToken(deviceToken);
+    }
+}
