935c6dd486db3bafe93844c19618dd587123c5d3
hyotatoFrappuccino <hjkiln@naver.com>
2025-12-24 13:57:22 +0900

    ♻️ refactor: 디바이스 토큰 unique 제약사항 추가

--- src/main/java/im/swyp/teumteumeat/domains/notification/persistence/entity/DeviceToken.java
+++ src/main/java/im/swyp/teumteumeat/domains/notification/persistence/entity/DeviceToken.java
@@ -24,7 +24,7 @@ public class DeviceToken extends BaseEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String token;
 
     @Column(nullable = false)
