1cf5a92f984ae65a6d70747809d4cf1fa8190b68
jinnieusLab <kitty14904@naver.com>
2025-12-28 14:40:44 +0900

    :art: style: Swagger 명세 분리

--- src/main/java/im/swyp/teumteumeat/domains/user/presentation/controller/UserWithdrawalController.java
+++ src/main/java/im/swyp/teumteumeat/domains/user/presentation/controller/UserWithdrawalController.java
@@ -5,8 +5,7 @@ import im.swyp.teumteumeat.domains.user.application.usecase.UserWithdrawalUseCas
 import im.swyp.teumteumeat.global.common.ApiResponse;
 import im.swyp.teumteumeat.global.common.CommonResponseCode;
 import im.swyp.teumteumeat.global.security.dto.CustomUserDetails;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import im.swyp.teumteumeat.domains.user.presentation.api.UserWithdrawalApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,15 +14,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "User Withdrawal", description = "회원 탈퇴 API")
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
-public class UserWithdrawalController {
+public class UserWithdrawalController implements UserWithdrawalApi {
 
     private final UserWithdrawalUseCase userWithdrawalUseCase;
 
-    @Operation(summary = "회원 탈퇴", description = "애플리케이션에서 회원을 탈퇴시키고 소셜 로그인 연결을 해제합니다.")
+    @Override
     @DeleteMapping("/withdrawal")
     public ResponseEntity<ApiResponse<Void>> withdraw(
             @AuthenticationPrincipal CustomUserDetails userDetails,
