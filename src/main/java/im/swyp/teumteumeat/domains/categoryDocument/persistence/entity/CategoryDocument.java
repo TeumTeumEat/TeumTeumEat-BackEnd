42fa6a3780de8d62c6943fb25ae2697bf998ecb7
jinnieusLab <kitty14904@naver.com>
2025-12-29 22:59:15 +0900

    :sparkles: feat: 퀴즈 개인화를 위해 CategoryDocument와 Goal 연동

--- src/main/java/im/swyp/teumteumeat/domains/categoryDocument/persistence/entity/CategoryDocument.java
+++ src/main/java/im/swyp/teumteumeat/domains/categoryDocument/persistence/entity/CategoryDocument.java
@@ -2,6 +2,7 @@ package im.swyp.teumteumeat.domains.categoryDocument.persistence.entity;
 
 import im.swyp.teumteumeat.domains.category.persistence.entity.Category;
 import im.swyp.teumteumeat.global.base.entity.BaseEntity;
+import im.swyp.teumteumeat.domains.goal.persistence.entity.Goal;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -28,10 +29,15 @@ public class CategoryDocument extends BaseEntity {
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "goal_id")
+    private Goal goal;
+
     @Builder
-    public CategoryDocument(String content, String title, Category category) {
+    public CategoryDocument(String content, String title, Category category, Goal goal) {
         this.content = content;
         this.title = title;
         this.category = category;
+        this.goal = goal;
     }
 }
