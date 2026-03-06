793c5974abda69310932421dd508d965a17fbb7d
hyotatoFrappuccino <hjkiln@naver.com>
2026-03-06 12:18:52 +0900

    ✨ feat: EmitterRepository 구현

---
+++ src/main/java/im/swyp/teumteumeat/global/sse/EmitterRepository.java
@@ -0,0 +1,21 @@
+package im.swyp.teumteumeat.global.sse;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import java.util.Map;
+
+public interface EmitterRepository {
+    /* Emitter */
+    void save(String id, SseEmitter emitter);
+    
+    // 특정 식별자로 시작하는 모든 Emitter 조회
+    Map<String, SseEmitter> findAllEmitterStartWithById(String id);
+    
+    void deleteById(String id);
+
+    /* Event Cache */
+    // 전송한 이벤트를 캐시에 저장
+    void saveEventCache(String id, Object event);
+
+    // 특정 식별자로 시작하는 모든 이벤트 캐시 조회 (유실 데이터 조회용)
+    Map<String, Object> findAllEventCacheStartWithById(String id);
+}
---
+++ src/main/java/im/swyp/teumteumeat/global/sse/InMemoryEmitterRepository.java
@@ -0,0 +1,49 @@
+package im.swyp.teumteumeat.global.sse;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Repository
+public class InMemoryEmitterRepository implements EmitterRepository {
+
+    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
+
+    /* Emitter */
+    @Override
+    public void save(String id, SseEmitter emitter) {
+        emitters.put(id, emitter);
+    }
+
+    @Override
+    public Map<String, SseEmitter> findAllEmitterStartWithById(String id) {
+        return findAllStartWithById(emitters, id);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        emitters.remove(id);
+    }
+
+    /* Event Cache */
+    @Override
+    public void saveEventCache(String id, Object event) {
+        eventCache.put(id, event);
+    }
+
+    @Override
+    public Map<String, Object> findAllEventCacheStartWithById(String id) {
+        return findAllStartWithById(eventCache, id);
+    }
+
+    /* HELPER METHOD */
+    private <T> Map<String, T> findAllStartWithById(Map<String, T> map, String id) {
+        return map.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(id))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
