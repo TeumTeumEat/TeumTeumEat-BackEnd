0572fe1b34391cba54e1b7e08330d098c45f993f
hyotatoFrappuccino <hjkiln@naver.com>
2026-03-08 12:34:55 +0900

    ♻️ refactor: GlobalExceptionHandler 예외 응답 미디어 타입 일관성 확보
    - 기존에는 클라이언트의 Accept헤더에 따라 응답 헤더가 결정되어 406 Not Acceptable 예외가 발생하여 응답 헤더를 json형식으로 통일해주었습니다.

--- src/main/java/im/swyp/teumteumeat/global/exception/GlobalExceptionHandler.java
+++ src/main/java/im/swyp/teumteumeat/global/exception/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
@@ -19,7 +20,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
-
 
 @Slf4j
 @RestControllerAdvice
@@ -38,54 +38,79 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             log.info("BaseException: {}", e.getMessage());
         }
 
-        return new ResponseEntity<>(ApiResponse.ofFail(responseCode), responseCode.getStatus());
+        return ResponseEntity
+                .status(responseCode.getStatus())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ApiResponse.ofFail(responseCode));
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ApiResponse<Void>> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
         log.error("DataIntegrityViolationException: ", e);
         BaseResponseCode responseCode = CommonResponseCode.DATA_INTEGRITY_VIOLATION;
-        return new ResponseEntity<>(ApiResponse.ofFail(responseCode), responseCode.getStatus());
+
+        return ResponseEntity
+                .status(responseCode.getStatus())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ApiResponse.ofFail(responseCode));
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ApiResponse<Void>> handleAccessDeniedException(AccessDeniedException e) {
         log.error("AccessDeniedException: ", e);
         BaseResponseCode responseCode = CommonResponseCode.FORBIDDEN;
-        return new ResponseEntity<>(ApiResponse.ofFail(responseCode), responseCode.getStatus());
+        return ResponseEntity
+                .status(responseCode.getStatus())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ApiResponse.ofFail(responseCode));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
         log.error("Exception: ", e);
         BaseResponseCode responseCode = CommonResponseCode.INTERNAL_SERVER_ERROR;
-        return new ResponseEntity<>(ApiResponse.ofFail(responseCode), responseCode.getStatus());
+        return ResponseEntity
+                .status(responseCode.getStatus())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ApiResponse.ofFail(responseCode));
     }
 
     @Override
     protected ResponseEntity<Object> handleNoResourceFoundException(NoResourceFoundException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         BaseResponseCode responseCode = CommonResponseCode.NOT_FOUND;
-        return new ResponseEntity<>(ApiResponse.ofFail(responseCode), status);
+        return ResponseEntity
+                .status(responseCode.getStatus())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ApiResponse.ofFail(responseCode));
     }
 
     @Override
     protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         log.error("HttpRequestMethodNotSupportedException: ", e);
         BaseResponseCode responseCode = CommonResponseCode.METHOD_NOT_ALLOWED;
-        return new ResponseEntity<>(ApiResponse.ofFail(responseCode), status);
+        return ResponseEntity
+                .status(responseCode.getStatus())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ApiResponse.ofFail(responseCode));
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         log.error("MethodArgumentNotValidException: ", e);
         BaseResponseCode responseCode = CommonResponseCode.INVALID_METHOD_ARGUMENT;
-        return new ResponseEntity<>(ApiResponse.ofFail(responseCode, e.getBindingResult().getFieldErrors()), status);
+        return ResponseEntity
+                .status(responseCode.getStatus())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ApiResponse.ofFail(responseCode, e.getBindingResult().getFieldErrors()));
     }
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         log.error("JSON Parse Error: ", e);
         BaseResponseCode responseCode = CommonResponseCode.BAD_REQUEST;
-        return new ResponseEntity<>(ApiResponse.ofFail(responseCode), responseCode.getStatus());
+        return ResponseEntity
+                .status(responseCode.getStatus())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ApiResponse.ofFail(responseCode));
     }
 }
