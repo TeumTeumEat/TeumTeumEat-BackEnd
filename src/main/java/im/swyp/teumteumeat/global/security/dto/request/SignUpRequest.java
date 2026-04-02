b986bd27dc7c76e888eca680a680eac9d4dde463
hyotatoFrappuccino <hjkiln@naver.com>
2026-04-02 23:45:56 +0900

    ♻️ refactor: /register API에 이메일 필드 추가

--- src/main/java/im/swyp/teumteumeat/global/security/dto/request/SignUpRequest.java
+++ src/main/java/im/swyp/teumteumeat/global/security/dto/request/SignUpRequest.java
@@ -14,6 +14,9 @@ public class SignUpRequest {
 
             @Schema(description = "유저 이름", example = "홍길동(애플 로그인 전용)") String name,
 
-            @Schema(description = "인증 코드 (Refresh Token 발급용)", example = "c8f2...") String authCode) {
+            @Schema(description = "인증 코드 (Refresh Token 발급용)", example = "c8f2...") String authCode,
+
+            @Schema(description = "이메일", example = "teumteumeat@gmail.com (애플 회원가입 전용") String email
+    ) {
     }
 }
