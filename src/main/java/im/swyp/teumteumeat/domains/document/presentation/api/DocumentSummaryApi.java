9cd5dc08aedc10c90504e676fbad742917a1241c
jinnieusLab <kitty14904@naver.com>
2026-04-06 17:12:36 +0900

    :memo: docs: DocumentSummary 생성 스트리밍 API 명세 추가

--- src/main/java/im/swyp/teumteumeat/domains/document/presentation/api/DocumentSummaryApi.java
+++ src/main/java/im/swyp/teumteumeat/domains/document/presentation/api/DocumentSummaryApi.java
@@ -25,20 +25,12 @@ public interface DocumentSummaryApi {
                         @PathVariable Long documentId,
                         @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails user);
 
-//        @Operation(summary = "PDF 요약글 및 퀴즈 생성 스트리밍 (학습 시작)", description = "PDF의 새로운 요약본과 퀴즈를 생성합니다. (결과는 비동기로 SSE를 통해 전달됨)")
-//        @ApiResponseExplanations(success = @ApiSuccessResponseExplanation(description = "생성 요청 성공 (결과는 비동기 SSE 알림)"))
-//        ResponseEntity<ApiResponse<Void>> createSummaryStream(
-//                        @PathVariable Long goalId,
-//                        @PathVariable Long documentId,
-//                        @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails user);
-
-//        @Operation(summary = "PDF 요약글 생성 SSE 구독", description = "생성 비동기 처리 결과를 받기 위한 SSE 스트림에 연결합니다.")
-//        SseEmitter subscribe(
-//                        @PathVariable Long goalId,
-//                        @PathVariable Long documentId,
-//                        @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails user,
-//                        @RequestHeader(value = "Last-Event-ID", required = false) String lastEventId,
-//                        HttpServletResponse response);
+        @Operation(summary = "스트리밍 방식 - PDF 요약글 및 퀴즈 생성 (학습 시작)", description = "PDF의 새로운 요약본과 퀴즈를  동기로 생성합니다.")
+        @ApiResponseExplanations(success = @ApiSuccessResponseExplanation(responseClass = DocumentDetailResponse.class, description = "생성 요청 성공"))
+        ResponseEntity<SseEmitter> createSummaryStream(
+                @PathVariable Long goalId,
+                @PathVariable Long documentId,
+                @Parameter(hidden = true) @AuthenticationPrincipal CustomUserDetails user);
 
         @Operation(summary = "PDF 요약글 단순 조회 (이어 읽기)", description = "가장 최근에 생성되어 진행 중인 PDF 요약글을 횟수 차감 없이 그대로 다시 조회합니다.")
         @ApiResponseExplanations(success = @ApiSuccessResponseExplanation(responseClass = DocumentDetailResponse.class, description = "조회 성공"))
