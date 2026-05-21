b06637e2ef2199aed698df56def57e4a9f71f00e
jinnieusLab <kitty14904@naver.com>
2026-05-21 12:23:28 +0900

    :recycle: refactor: LlmStreamProvider connect 이벤트명 통일 및 yml 원복

--- src/main/java/im/swyp/teumteumeat/global/sse/component/LlmStreamProvider.java
+++ src/main/java/im/swyp/teumteumeat/global/sse/component/LlmStreamProvider.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 public class LlmStreamProvider {
 
     private static final Long DEFAULT_STREAM_TIMEOUT = 10 * 60 * 1000L; // 10분
+    private static final String EVENT_NAME_CONNECT = "connect";
 
     public SseEmitter createStreamEmitter() {
         return createStreamEmitter(DEFAULT_STREAM_TIMEOUT);
@@ -22,7 +23,7 @@ public class LlmStreamProvider {
         SseEmitter emitter = new SseEmitter(timeout);
         try {
             // 프록시(Nginx 등) 503 에러 방지 및 연결 성공 알림을 위한 초기 이벤트
-            emitter.send(SseEmitter.event().name("CONNECT").data("Stream 연결"));
+            emitter.send(SseEmitter.event().name(EVENT_NAME_CONNECT).data("Stream 연결"));
         } catch (IOException e) {
             log.error("스트리밍 연결 초기화 중 에러 발생", e);
             emitter.completeWithError(e);
