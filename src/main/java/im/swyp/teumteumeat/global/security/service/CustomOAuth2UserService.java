bf8f7fc78d1554fe34e41c294ff17bcb80b43bd4
hyotatoFrappuccino <hjkiln0@gmail.com>
2026-09-07 11:20:22 +0900

    ♻️ refactor: 디버그 로그 레벨 debug로 변경

--- src/main/java/im/swyp/teumteumeat/global/security/service/CustomOAuth2UserService.java
+++ src/main/java/im/swyp/teumteumeat/global/security/service/CustomOAuth2UserService.java
@@ -33,10 +33,10 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Transactional
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        log.info("CustomOAuth2UserService.loadUser() executed");
+        log.debug("CustomOAuth2UserService.loadUser() executed");
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        log.info("Registration ID: {}", registrationId);
+        log.debug("Registration ID: {}", registrationId);
 
         Map<String, Object> attributes;
         String userNameAttributeName;
